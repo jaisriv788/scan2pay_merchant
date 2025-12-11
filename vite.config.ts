ee17552a3711341d0bd0ef516174ecd98999c772
jaisriv788 <srivastavajai845@gmail.com>
2025-12-11 13:48:23 +0530

    added the pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,63 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate", // auto-updates the service worker
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Scan2Pay Direct Merchant",
+        short_name: "Scan2Pay",
+        description: "Scan2Pay application.",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/merchant/",
+        start_url: "/merchant/",
+        icons: [
+          {
+            src: "three.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "four.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "four.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+
   base: "/merchant/",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
